f328cd715265c29213c2ef348664aac4063c41b2
Loenix <cartman34@users.noreply.github.com>
2026-05-12 13:32:25 +0200

    [backlog-rename-todo-list] Add PHPDoc on BacklogTodoListCommand methods

--- scripts/src/Backlog/Command/BacklogTodoListCommand.php
+++ scripts/src/Backlog/Command/BacklogTodoListCommand.php
@@ -16,11 +16,23 @@ use SoManAgent\Script\Backlog\Service\BacklogPresenter;
  */
 final class BacklogTodoListCommand extends AbstractBacklogCommand
 {
+    /**
+     * @param BacklogPresenter $presenter
+     * @param bool $dryRun
+     * @param string $projectRoot
+     * @param BacklogBoardService $boardService
+     */
     public function __construct(BacklogPresenter $presenter, bool $dryRun, string $projectRoot, BacklogBoardService $boardService)
     {
         parent::__construct($presenter, $dryRun, $projectRoot, $boardService);
     }
 
+    /**
+     * List queued todo tasks in priority order.
+     *
+     * @param list<string> $commandArgs
+     * @param array<string, bool|string|array<bool|string>> $options
+     */
     public function handle(array $commandArgs, array $options): void
     {
         $entries = $this->loadBoard()->getEntries(BacklogBoard::SECTION_TODO);
